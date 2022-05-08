87ed49d32d3be95853958832c4533a5bfb7e805e
Jeena Park <jeenapark15@gmail.com>
2022-05-08 18:59:18 -0400

    commit tree b562bf7d37a245bdc98725e5cb4711a05bcba3f9

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Add your code here
+function submitData(name, email) {
+
+    const formData = {
+        name: name, 
+        email: email, 
+    };
+
+    const configurationObject = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+        },
+        body: JSON.stringify(formData),
+    };
+
+    return fetch("http://localhost:3000/users", configurationObject)
+        .then(resp => resp.json())
+        .then(obj => {
+            const body = document.querySelector('body');
+            const span = document.createElement('span');
+
+            body.append(span);
+            span.append(obj.id);
+        })
+        .catch(error => {
+            alert("!");
+            console.log(error.message);
+            console.log("error message");
+
+            const body = document.querySelector('body');
+            const span = document.createElement('span');
+
+            body.append(span);
+            span.append(error.message);
+        });
+}  
+
